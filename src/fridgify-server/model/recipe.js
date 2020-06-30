f6c9bce279bbf09cf7e98af33d5722ed423f0957
daseyun <daseyun@gmail.com>
2020-06-29 22:54:00 -0400

    added `cuisine` to recipe schema

--- src/fridgify-server/model/recipe.js
+++ src/fridgify-server/model/recipe.js
@@ -11,6 +11,7 @@ let Recipe = new Schema({
   created: { type: Date },
   instruction: { type: String }, // save as entire HTML string with tags for formatting. ex: <html><p> add soy sauce </p> <p>add onions </p></html>
   items: [{ type: Schema.Types.ObjectId, ref: "Item" }], // ingredients
+  cuisine: { type: String },
   img: { data: Buffer, contentType: String }, // https://stackoverflow.com/questions/29780733/store-an-image-in-mongodb-using-node-js-express-and-mongoose
 });
 

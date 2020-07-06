17cf143e4ab625a5cab207460914d6ca8c4884c9
9710willy <myungjoon.lee@vanderbilt.edu>
2020-07-05 20:03:45 -0500

    backend-hot-reload

--- src/fridgify-server/App.js
+++ src/fridgify-server/App.js
@@ -55,6 +55,7 @@ const setupServer = async () => {
     // await app.models.Quantity.deleteMany({}, function (err) {});
     // return;
     await app.models.Quantity.countDocuments({}, function (err, count) {
+      if (err) console.log(err);
       console.log("[DEV]Quantity is:", count);
     });
     await app.models.Quantity.findOne({ symbol: "g" }, function (err, res) {

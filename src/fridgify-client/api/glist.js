b63e0fb498ca43608aa2d47f49f39457a4dc1ede
9710willy <myungjoon.lee@vanderbilt.edu>
2020-07-10 17:27:19 -0500

    api-client-update

--- src/fridgify-client/api/glist.js
+++ src/fridgify-client/api/glist.js
@@ -2,25 +2,24 @@ import client from "./client";
 
 const endpoint = "/glist";
 
-const getGlists = (token) => client.get(`${endpoint}/${token}`);
+const getGlists = () => client.get(endpoint);
 
 const getGlistById = (glistId) => client.get(`${endpoint}/id/${glistId}`);
 
-export const addGlist = (glistName, token) => {
-  return client.post(`${endpoint}/${token}`, glistName);
+export const addGlist = (glistName) => {
+  return client.post(endpoint, glistName);
 };
 
-export const deleteGlist = (glistId, token) => {
-  const obj = { data: { _id: glistId } };
-  return client.delete(`${endpoint}/${token}`, {}, obj);
+export const deleteGlist = (glistId) => {
+  return client.delete(endpoint, {}, glistId);
 };
 
-export const editGlist = (newFridge, token) => {
-  return client.put(`${endpoint}/${token}`, newFridge);
+export const editGlist = (newGlist) => {
+  return client.put(endpoint, newGlist);
 };
 
-export const submitGlist = () => {
-  return client.put();
+export const submitGlist = (data) => {
+  return client.put(`${endpoint}/fridge`, data);
 };
 
 export default {
--- src/fridgify-client/api/item.js
+++ src/fridgify-client/api/item.js
@@ -4,32 +4,32 @@ const endpoint = "/item";
 const itemFridgeEndpoint = "/item/fridge";
 const itemGlistEndpoint = "/item/glist";
 
-export const getFridgeItems = (fridgeId, token) => {
-  return client.get(`${itemFridgeEndpoint}/${fridgeId}/${token}`);
+export const getFridgeItems = (fridgeId) => {
+  return client.get(`${itemFridgeEndpoint}/${fridgeId}`);
 };
 
-export const getGlistItems = (glistId, token) => {
-  return client.get(`${itemGlistEndpoint}/${glistId}/${token}`);
+export const getGlistItems = (glistId) => {
+  return client.get(`${itemGlistEndpoint}/${glistId}`);
 };
 
-export const addFridgeItem = (data, token) => {
-  return client.post(`${itemFridgeEndpoint}/${token}`, data);
+export const addFridgeItem = (data) => {
+  return client.post(`${itemFridgeEndpoint}`, data);
 };
 
-export const addGlistItem = (data, token) => {
-  return client.post(`${itemGlistEndpoint}/${token}`, data);
+export const addGlistItem = (data) => {
+  return client.post(`${itemGlistEndpoint}`, data);
 };
 
-export const deleteFridgeItem = (data, token) => {
-  return client.delete(`${itemFridgeEndpoint}/${token}`, {}, data);
+export const deleteFridgeItem = (data) => {
+  return client.delete(`${itemFridgeEndpoint}`, {}, data);
 };
 
-export const deleteGlistItem = (data, token) => {
-  return client.delete(`${itemGlistEndpoint}/${token}`, {}, data);
+export const deleteGlistItem = (data) => {
+  return client.delete(`${itemGlistEndpoint}`, {}, data);
 };
 
-export const editItem = (data, token) => {
-  return client.put(`${endpoint}/${token}`, data);
+export const editItem = (data) => {
+  return client.put(`${endpoint}`, data);
 };
 
 export default {

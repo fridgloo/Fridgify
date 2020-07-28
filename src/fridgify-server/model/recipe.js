27b97cb81408a64075ae4f3afb02a85a51de5ee6
daseyun <daseyun@gmail.com>
2020-07-27 21:30:18 -0400

    isUserCreated field added in Recipe

--- src/fridgify-server/model/recipe.js
+++ src/fridgify-server/model/recipe.js
@@ -12,6 +12,7 @@ let Recipe = new Schema({
   instructions: { type: String }, // save as entire HTML string with tags for formatting. ex: <html><p> add soy sauce </p> <p>add onions </p></html>
   items: [{ type: Schema.Types.ObjectId, ref: "Recipe_Item_Idx" }], // ingredients
   cuisine: { type: String },
+  isUserCreated: { type: Boolean, default: true },
   // maybe have an asset folder for storing images ?
   // and just make img the filepath to display
 });
